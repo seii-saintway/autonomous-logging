2ca218568e2013f421fbca348ca13d32a26881d1
saintway @ Andrews-MacBook-Pro <saintway.sakaru@gmail.com>
2021-03-16 15:42:03 +0900

    logging.root.manager.loggerDict is useful

--- my_test_logging.py
+++ my_test_logging.py
@@ -1,13 +1,16 @@
 
 import logging
 
-print(logging.root.manager.loggerDict)
+print(f'logging.root.manager.loggerDict = {logging.root.manager.loggerDict}')
 
 logger = logging.getLogger('test_logging')
+print(f'logging.root.manager.loggerDict = {logging.root.manager.loggerDict}')
+
 logger.setLevel(logging.DEBUG)
 
 logger.addHandler(logging.FileHandler('my_test_logging.log'))
 
 if __name__ == '__main__':
     import test_logging
-    print(test_logging.__name__)
+    print(f'test_logging.__name__ = {test_logging.__name__}')
+    print(f'logging.root.manager.loggerDict = {logging.root.manager.loggerDict}')
